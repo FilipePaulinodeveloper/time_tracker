d3d721f3df8910edee7ed70d73535c26630b8620
Filipe Paulino <filipepaulinodeveloper@hotmail.com>
2023-01-13 13:33:12 -0300

    Commit

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,13 +1,48 @@
 <template>
-    <h1>Projetos</h1>
+    <section class="projetos">
+        <h1 class="title">Projetos</h1>
+        <form @submit.prevent="salvar">
+            <div class="field">
+                <label for="nomeDoProjeto" class="label">
+                    Nome do projeto
+                </label>
+                <input 
+                 type="text"
+                 class="input" 
+                 v-model="nomeDoProjeto"
+                 id="nomeDoProjeto"
+                />
+            </div>
+            <div class="field">
+                <button class="button" type="submit">
+                    Salvar
+                </button>
+            </div>
+        </form>
+    </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import IProjeto from '../interfaces/projeto'
 
 export default defineComponent (
 {
-     name: "ProjeTos"
+     name: "ProjeTos",
+
+     data() {
+        return {
+            nomeDoProjeto:''
+        }
+     },
+     methods:{
+        salvar() {
+            const Projeto : IProjeto = {
+                nome:this.nomeDoProjeto,
+                id: new Date().toISOString
+            }
+        }
+     }
 })
 </script>
 
