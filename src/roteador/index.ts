69add4b830b7dbed4893ec9ae728324f4f2d5c87
Filipe Paulino <filipepaulinodeveloper@hotmail.com>
2023-01-26 22:50:11 -0300

    Redirecionando para editar

--- src/roteador/index.ts
+++ src/roteador/index.ts
@@ -1,6 +1,7 @@
 import {createRouter,createWebHashHistory,RouteRecordRaw} from "vue-router";
 import Tarefas from '../views/Tarefas.vue'
 import Projetos from '../views/Projetos.vue'
+import Formulario from '../views/projetos/FormularioProject.vue'
 const rotas: RouteRecordRaw[] = [
 
     {
@@ -12,6 +13,16 @@ const rotas: RouteRecordRaw[] = [
         path: '/projetos',
         name:'ProjeTos',
         component:Projetos   
+    },
+    {
+        path: '/projetos/novo',
+        name:' Nome Projeto ',
+        component:Formulario 
+    },
+    {
+        path: '/projetos/id',
+        name:' Editar Projeto ',
+        component:Formulario 
     },
 ]
 
---
+++ src/views/projetos/FormularioProject.vue
@@ -0,0 +1,61 @@
+<template>
+    <section class="projetos">
+        <h1 class="title">Projetos</h1>
+        <form @submit.prevent="salvar">
+            <div class="field">
+                <label for="nomeDoProjeto" class="label">
+                    Nome do projeto
+                </label>
+                <input 
+                 type="text"
+                 class="input" 
+                 v-model="nomeDoProjeto"
+                 id="nomeDoProjeto"
+                />
+            </div>
+            <div class="field">
+                <button class="button" type="submit">
+                    Salvar
+                </button>
+            </div>
+        </form>
+    </section>
+</template>
+
+<script lang="ts">
+import { useStore } from "@/store";
+import { defineComponent, computed } from "vue";
+
+
+export default defineComponent (
+{
+     name: "FormularioProject",
+
+     data() {
+        return {
+            nomeDoProjeto:'',            
+        }
+     },
+     methods:{
+        salvar() {
+            this.store.commit('ADICIONA_PROJETO',this.nomeDoProjeto)           
+            this.nomeDoProjeto = ''
+            this.$router.push('/projetos')
+        }        
+     },
+     setup(){
+        const store = useStore()
+        return{
+            store,
+            projetos: computed(()=> store.state.projetos)
+        }
+     }
+})
+</script>
+
+<style scoped>
+.projetos{
+    padding: 1.25rem;
+}
+
+</style>
